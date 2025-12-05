b435b78fa300e4d3152ae7ed577e61ca8e5a328c
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-12-05 16:23:27 -0500

    env

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
         Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzBiN2NmY2E2NzEzNjIzY2VhNWNjOSIsInVzZXJuYW1lIjoiYWJlIiwiZW1haWwiOiJhYmVAZ21haWwuY29tIiwicm9sZSI6InVzZXIifSwiaWF0IjoxNzY0OTU2MjkxLCJleHAiOjE3NjQ5NjM0OTF9.P6WN68BTrBlzx3BbynPMJAahxAHFA2C7F4_3-CwbkmA"
     }
